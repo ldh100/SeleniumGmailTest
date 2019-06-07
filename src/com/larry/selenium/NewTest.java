31a63f1a5bee92b265fe537dd7494dec5ae27bea
ldh100 <ldh100@sina.com>
2019-06-06 22:08:12 -0400

    1.3

--- src/com/larry/selenium/NewTest.java
+++ src/com/larry/selenium/NewTest.java
@@ -5,14 +5,20 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
+import static org.testng.Assert.assertEqualsDeep;
+import static org.testng.Assert.assertTrue;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
@@ -22,8 +28,6 @@ import org.testng.annotations.AfterSuite;
 public class NewTest {
 	private WebDriver driver;
 	private String baseUrl;
-
-	
 
 	@BeforeMethod
 	public void beforeMethod() {
@@ -38,36 +42,47 @@ public class NewTest {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
 		ChromeOptions Options = new ChromeOptions();
-		// Options.addArguments("user-data-dir=C:\\Users\\iamld\\AppData\\Local\\Google\\Chrome\\User
-		// Data\\");
+	
 		driver = new ChromeDriver(Options);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 	}
+
 	@Test
 	public void Test() {
-		String baseUrl = "https://accounts.google.com/signin";
-		String accountName ="versapay.qa.test@gmail.com";
+		String baseUrl = "http://gmail.google.com";
+		String accountName = "versapay.qa.test@gmail.com ";
 		String password = "iLoveApples";
-        driver.get(baseUrl);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        //WebDriverWait wait=new WebDriverWait(driver, 20);
-        driver.findElement(By.id("identifierId")).sendKeys(accountName, Keys.ENTER);                       
-        //  driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();         
+		driver.get(baseUrl);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		driver.findElement(By.id("identifierId")).sendKeys(accountName, Keys.ENTER);
+	
+		driver.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(password, Keys.ENTER);
 
-        //driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys(password,Keys.ENTER);             
-        driver.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(password);
-          driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);    
-         
-          driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
-		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		String titleString = driver.getTitle();
+		System.out.println(titleString);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	
+		Assert.assertTrue(titleString.contains(accountName));
+		// Assert.assertt
+		// Assert.assertEquals(accountName,
+		// titl(eString.substring(titleString.length()-accountName.length()));
+
 	}
 
 	@AfterClass
 	public void afterClass() {
 
-       // driver.close();
-       // driver.quit();
+		 driver.close();
+		 driver.quit();
 	}
 
 	@BeforeTest
